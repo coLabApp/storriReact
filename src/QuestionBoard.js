4fa03ac7a19cb3592e7edb73ad913ba017c4a2e4
Abidu1 <abidusra@gmail.com>
2021-03-25 19:53:44 -0400

    added component

--- src/QuestionBoard.js
+++ src/QuestionBoard.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import StoryBoard from './StoryBoard';
 
 function QuestionBoard() {
 
@@ -82,15 +83,14 @@ function QuestionBoard() {
         const newStyle = progressPercentage + 14;
         const display = toggleState + "none"
         const show = showContainer + "grid"
-        if(userInput[1] !== ""){
-            setUserInput([...userInput, newVar]) 
-        }  else {setResponse([...response, questions[currentQuestionId]])}
+        // if(userInput[1] !== ""){
+        //     setUserInput([...userInput, newVar]) 
+        // }  else {setResponse([...response, questions[currentQuestionId]])}
 
        
         if (nextId > 7){
             setToggleState(display)
             setShowContainer(show)
-
         }
         setQuestion(nextId)
         setWidth(newStyle)
@@ -129,83 +129,11 @@ function QuestionBoard() {
                     </div>
                     </div>
                 </div>
-
+            </div>
         </div>
-        <div className="mainSection"style={{display: showContainer}}>
-        <main>
-          
-        <section>
-            <div className="sectionContainer">
-                <div className="storyBoard">
-                    <div className="wrapper">
-                        <h2>Writer's Room</h2>
-                        <div className="saveContainer "id="saveContainer">
-                            <div className="userAnswer">{userInput.map((uservalue, i) =>{
-                                return (
-                                    <div key={i}>
-                                <div key={uservalue.id}>
-                                <input value={uservalue[0]}></input>
-                                <textarea value={uservalue[1]}></textarea>  
-                                </div>
-                                </div>
-
-                                )
-                                
-                            })}
-                                </div>                      
-                        </div>
-                    </div>
-                </div>
-                <div className="storyContainer">
-                    <div className="wrapper">
-                        <h2>Blank Cards</h2>
-                        <div className="blankCards" id="blankCards">
-                            <div className="userAnswer">{response.map((item,i) =>{
-                                return(
-                                    <div className="wrapper" key={i}>
-                                <div key={item.id}>
-                                    <input type="text" value={item.question}></input>
-                                    <textarea></textarea>
-                                </div>
-                                </div>
-
-                                )
-                                
-                            })}</div>                           
-                        </div>
-                    </div>
-                </div>
-                
-                <div className="storyContainer">
-                    <div className="wrapper">
-                        <h2>Beginning</h2>
-                        <div className="storyBoard">
-
-                        </div>
-                    </div>
-                </div>
-        
-                <div className="storyContainer">
-                    <div className="wrapper">
-                        <h2>Middle</h2>
-                        <div className="storyBoard">
-                            
-                        </div>
-                    </div>
-                </div>
-        
-                <div className="storyContainer">
-                    <div className="wrapper">
-                        <h2>End</h2>
-                        <div className="storyBoard">
-                        </div>
-                    </div>
-                </div>
-            </div>   
+        <section className="storyBoardSection" style={{display: showContainer}}>
+        <StoryBoard  userInput={userInput}/>
         </section>
-    </main>
-    </div>
-        </div>
     </div>
   )
 }
--- src/StoryBoard.js
+++ src/StoryBoard.js
@@ -1,28 +1,98 @@
 import './App.css';
 import { useState, useEffect, useRef } from 'react';
 // import Main from './Main'
-// import QuestionBoard from './QuestionBoard'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
-function StoryBoard() {
+function StoryBoard(props) {
 
-  const [show, setShow] = useState(false)
-  const [display, setDisplay] = useState('')
-
-  const newStory = () =>{
-    const changeDisplay = display + "none";
-    setShow(!show)
-    if(!show){
-      setDisplay(changeDisplay)
-    }
-
-  }
 
   return (
-    <div className="StoryBOARD">
+    <div className="Storyboard">
+       <main>
+          <section>
+              <div className="sectionContainer">
+                {(() =>{
+                  if(props.userInput[1] !== ""){
+                    <div className="storyBoard">
+                    <div className="wrapper">
+                        <h2>Writer's Room</h2>
+                        <div className="saveContainer "id="saveContainer">
+                            <div className="userAnswer">{props.userInput.map((uservalue, i) =>{
+                                return (
+                                    <div key={i}>
+                                <div key={uservalue.id}>
+                                <input value={uservalue[0]}></input>
+                                <textarea value={uservalue[1]}></textarea>  
+                                </div>
+                                </div>
+
+                                )
+                                
+                            })}
+                                </div>                      
+                        </div>
+                    </div>
+                </div>
+
+                  } else {
+                    <div className="storyContainer">
+                    <div className="wrapper">
+                        <h2>Blank Cards</h2>
+                        <div className="blankCards" id="blankCards">
+                            <div className="userAnswer">{props.userInput.map((item,i) =>{
+                                return(
+                                    <div className="wrapper" key={i}>
+                                <div key={item.id}>
+                                    <input type="text" value={item[0]}></input>
+                                    <textarea></textarea>
+                                </div>
+                                </div>
+
+                                )
+                                
+                            })}</div>                           
+                        </div>
+                    </div>
+                </div> 
+                  }
+                })}
+                  
+
+                  
+                  <div className="storyContainer">
+                      <div className="wrapper">
+                          <h2>Beginning</h2>
+                          <div className="storyBoard">
+  
+                          </div>
+                      </div>
+                  </div>
+          
+                  <div className="storyContainer">
+                      <div className="wrapper">
+                          <h2>Middle</h2>
+                          <div className="storyBoard">
+                              
+                          </div>
+                      </div>
+                  </div>
+          
+                  <div className="storyContainer">
+                      <div className="wrapper">
+                          <h2>End</h2>
+                          <div className="storyBoard">
+                          </div>
+                      </div>
+                  </div>
+              </div>   
+          </section>
+      </main>
+
+
 
       
     </div>
   );
 }
 
-export default Storyboard;
+export default StoryBoard;
